cd046ff3d54d9a0742ea77403b8c7cd7feceeaf5
yogesh.patil <yogesh.patil@wonderlendhubs.com>
2025-04-08 22:41:15 +0530

    New changes

---
+++ _next/static/chunks/app/blog/[slug]/page-e8ac9a7e48d571aa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[953],{107:()=>{},3095:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,107,23)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[266,874,441,684,358],()=>s(3095)),_N_E=e.O()}]);
